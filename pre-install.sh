d88d0a9df2295f4cc6e4310fc9569eda9d350060
Ilya Beltsiukou <968597@gmail.com>
2021-07-12 15:32:56 +0300

    init

---
+++ pre-install.sh
@@ -0,0 +1,5 @@
+timedatectl set-ntp true
+pacman -Syy --noconfirm reflector openssh
+echo root:toor | chpasswd
+systemctl start openssh
+reflector -c Poland -a 6 --sort rate --save /etc/pacman.d/mirrorlists
